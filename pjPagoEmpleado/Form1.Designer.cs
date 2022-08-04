69efb317fe71e7fc26e48d0b6c854dce7cd92ecc
JADPA-06 <JADPA-06@JADPA-06>
2022-08-04 11:52:50 -0600

    Agregar archivos de proyecto.

---
+++ pjPagoEmpleado/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace pjPagoEmpleado
+{
+    partial class frmPagoDeEmpleados
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblMontobruto = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblMontoDescuento = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblMontoNeto = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(239, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Control de pago de empleado";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Empleado";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(604, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Horas";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(522, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Costo";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(601, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(143, 23);
+            this.textBox3.TabIndex = 6;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(57, 183);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 7;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(601, 183);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 8;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblMontoNeto);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.lblMontoDescuento);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.lblMontobruto);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(57, 233);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(721, 149);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "cuadro de resumen";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Monto bruto";
+            // 
+            // lblMontobruto
+            // 
+            this.lblMontobruto.AutoSize = true;
+            this.lblMontobruto.Location = new System.Drawing.Point(48, 92);
+            this.lblMontobruto.Name = "lblMontobruto";
+            this.lblMontobruto.Size = new System.Drawing.Size(38, 15);
+            this.lblMontobruto.TabIndex = 1;
+            this.lblMontobruto.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(317, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 15);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Monto descuento";
+            // 
+            // lblMontoDescuento
+            // 
+            this.lblMontoDescuento.AutoSize = true;
+            this.lblMontoDescuento.Location = new System.Drawing.Point(351, 92);
+            this.lblMontoDescuento.Name = "lblMontoDescuento";
+            this.lblMontoDescuento.Size = new System.Drawing.Size(38, 15);
+            this.lblMontoDescuento.TabIndex = 3;
+            this.lblMontoDescuento.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(617, 40);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 15);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Monto neto";
+            // 
+            // lblMontoNeto
+            // 
+            this.lblMontoNeto.AutoSize = true;
+            this.lblMontoNeto.Location = new System.Drawing.Point(624, 92);
+            this.lblMontoNeto.Name = "lblMontoNeto";
+            this.lblMontoNeto.Size = new System.Drawing.Size(44, 15);
+            this.lblMontoNeto.TabIndex = 5;
+            this.lblMontoNeto.Text = "label10";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(57, 430);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frmPagoDeEmpleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 486);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPagoDeEmpleados";
+            this.Text = "Pago de empleados";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private Button btnProcesar;
+        private Button btnLimpiar;
+        private GroupBox groupBox1;
+        private Label lblMontoNeto;
+        private Label label9;
+        private Label lblMontoDescuento;
+        private Label label7;
+        private Label lblMontobruto;
+        private Label label5;
+        private Button btnSalir;
+    }
+}
---
+++ pjPagoEmpleado/Form1.cs
@@ -0,0 +1,15 @@
+namespace pjPagoEmpleado
+{
+    public partial class frmPagoDeEmpleados : Form
+    {
+        public frmPagoDeEmpleados()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
